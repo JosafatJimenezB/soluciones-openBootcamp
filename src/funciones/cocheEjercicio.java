5a73daa62199a365639eee8c1b06659cc2f86ef4
josafat <josa221001@gmail.com>
2022-09-20 20:12:49 -0500

    add: añadiendo solucion ejercicios funciones

---
+++ src/funciones/cocheEjercicio.java
@@ -0,0 +1,22 @@
+package funciones;
+
+public class cocheEjercicio {
+
+    public static void main(String[] args) {
+
+        Coche miCoche = new Coche();
+
+        miCoche.agregarPuerta();
+        miCoche.agregarPuerta();
+        System.out.println(miCoche.puertas);
+    }
+
+}
+class Coche{
+    public int puertas = 0;
+
+    public void agregarPuerta(){
+        this.puertas++;
+    }
+
+}
---
+++ src/funciones/funcionesEjercicios.java
@@ -0,0 +1,14 @@
+package funciones;
+
+public class funcionesEjercicios {
+
+    public static void main(String[] args) {
+
+        int resultado = sumar(5, 10, 15);
+        System.out.println("suma = " + resultado);
+    }
+
+    public static int sumar(int a, int b, int c) {
+        return a + b + c;
+    }
+}
